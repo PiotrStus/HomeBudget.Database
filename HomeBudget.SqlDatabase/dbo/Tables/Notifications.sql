a00255fe74e724bba0c9ef207e4c967591985f0e
Piotr Stus <pm2024love@gmail.com>
2024-10-07 18:38:44 +0200

    AccountId added.

--- HomeBudget.SqlDatabase/dbo/Tables/Notifications.sql
+++ HomeBudget.SqlDatabase/dbo/Tables/Notifications.sql
@@ -2,6 +2,7 @@
 (
 	[Id]				INT					NOT NULL IDENTITY(1,1),
 	[UserId]			INT					NOT NULL,
+	[AccountId]			INT					NOT NULL,
 	[Date]				DATETIMEOFFSET		NOT NULL,
 	[Content]			NVARCHAR(100)		NOT NULL,
 	[IsRead]			BIT					NOT NULL CONSTRAINT [IsRead] DEFAULT 0,
@@ -9,9 +10,13 @@
 	[CategoryName]			NVARCHAR(50)		NOT NULL,
 	
 	CONSTRAINT [PK_Notifications] PRIMARY KEY CLUSTERED ([Id] ASC),
-	CONSTRAINT [FK_Notifications_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id]) ON DELETE CASCADE
+	CONSTRAINT [FK_Notifications_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id]) ON DELETE CASCADE,
+	CONSTRAINT [FK_Notifications_Accounts] FOREIGN KEY ([AccountId]) REFERENCES [dbo].[Accounts] ([Id]) ON DELETE CASCADE
 );
 
 GO
-CREATE INDEX [IX_Notifications_AccountId] ON [dbo].[Users] ([Id]);
+CREATE INDEX [IX_Notifications_UserId] ON [dbo].[Users] ([Id]);
+
+GO
+CREATE INDEX [IX_Notifications_AccountId] ON [dbo].[Accounts] ([Id]);
 
