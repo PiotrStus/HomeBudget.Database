777cd732785c28c2c3a1ed42698f1e8b2f9c8ccd
Piotr Stus <pm2024love@gmail.com>
2024-10-11 00:56:31 +0200

    AccountUsers: IsAdmin and it's constraint added.

--- HomeBudget.SqlDatabase/dbo/Tables/AccountUsers.sql
+++ HomeBudget.SqlDatabase/dbo/Tables/AccountUsers.sql
@@ -1,8 +1,9 @@
 ﻿CREATE TABLE [dbo].[AccountUsers]
 (
-    [Id] INT IDENTITY (1, 1) NOT NULL, 
-    [AccountId] INT NOT NULL,
-	[UserId] INT NOT NULL, 
+    [Id]            INT IDENTITY (1, 1) NOT NULL, 
+    [AccountId]     INT                 NOT NULL,
+	[UserId]        INT                 NOT NULL, 
+    [IsAdmin]       BIT				    NOT NULL CONSTRAINT [DF_IsAdmin] DEFAULT 0,
     -- on delete cascade to znaczy, ze podczas np. usuwania wiersza z tabeli accounts, automatycznie powiazany wiersz z tabli users zostanie usuniety
     CONSTRAINT [PK_AccountUsers] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_AccountUsers_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id]) ON DELETE CASCADE,
@@ -15,3 +16,6 @@ CREATE INDEX [IX_AccountUsers_AccountId] ON [dbo].[AccountUsers] ([AccountId])
 
 GO
 CREATE INDEX [IX_AccountUsers_UserId] ON [dbo].[AccountUsers] ([UserId])
+
+GO
+CREATE INDEX [IX_AccountUsers_IsAdmin] ON [dbo].[AccountUsers] ([IsAdmin])
