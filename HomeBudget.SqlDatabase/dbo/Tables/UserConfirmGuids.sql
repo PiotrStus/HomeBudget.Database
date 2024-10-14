bb624bcd156e9ce740b04f41306ad24371750117
Piotr Stus <pm2024love@gmail.com>
2024-10-14 20:13:56 +0200

    UserConfirmGuids table added, Users: IsActivated flag and it's index created.

---
+++ HomeBudget.SqlDatabase/dbo/Tables/UserConfirmGuids.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[UserConfirmGuids]
+(
+    [Id]				INT				IDENTITY (1, 1) NOT NULL,
+    [UserId]            INT                             NOT NULL, 
+    [UserGuid]          UNIQUEIDENTIFIER                NOT NULL,
+    [GuidType]          INT                             NOT NULL,
+    CONSTRAINT [PK_UserConfirmGuids] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_UserConfirmGuids_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id]) ON DELETE CASCADE,
+);
+
+
+GO
+CREATE INDEX [IX_UserConfirmGuids_UserId] ON [dbo].[UserConfirmGuids] ([UserId]);
+
+GO
+CREATE INDEX [IX_UserConfirmGuids_UserGuid] ON [dbo].[UserConfirmGuids] ([UserGuid]);
+
+GO
+CREATE INDEX [IX_UserConfirmGuids_GuidType] ON [dbo].[UserConfirmGuids] ([GuidType]);
--- HomeBudget.SqlDatabase/dbo/Tables/Users.sql
+++ HomeBudget.SqlDatabase/dbo/Tables/Users.sql
@@ -4,6 +4,7 @@
 -- PRIMARY KEY CLUSTERED ([Id] ASC) -> kolumna Id jest kluczem glownym i indeksem klastrowym
 	[Id]				INT				IDENTITY (1, 1) NOT NULL,
 	[Email]				NVARCHAR (100) NOT NULL,
+	[IsActivated]		BIT			   NOT NULL CONSTRAINT [DF_Users_IsDeleted] DEFAULT 0,
 	[HashedPassword]	NVARCHAR (200) NOT NULL,
 	[RegisterDate]		DATETIMEOFFSET NOT NULL,
 	CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
@@ -14,4 +15,7 @@
 GO
 
 -- indeks na email, dzieki czemu wyszukiwanie uzytkownikow bedzie szybsze
-CREATE UNIQUE INDEX [UQ_Users_Email] ON [dbo].[Users] ([Email])
+CREATE UNIQUE INDEX [UQ_Users_Email] ON [dbo].[Users] ([Email]);
+
+GO
+CREATE INDEX [IQ_Users_IsActivated] ON [dbo].[Users] ([IsActivated]);
